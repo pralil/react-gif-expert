1e7b86e2076010e3ae415ea9751b5f283f1db7de
Pablo <pablo.ralil@gmailexample.com>
2022-06-16 15:13:11 -0400

    Segundo commit

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -11,9 +11,7 @@ export const GifGrid = ({ category }) => {
         <>
             <h3>{ category }</h3>
             {
-                isLoading
-                ? ( <h2>Cargando...</h2> )
-                : null 
+                isLoading && ( <h2>Cargando...</h2> )
             }
             
             <div className="card-grid">
